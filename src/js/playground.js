85113de4bb373dc7b8f46c5716ea2738a567d8b0
Marlow Payne <marlow@mobify.com>
2016-06-01 17:41:24 -0700

    Add fixed-sticky option

--- src/js/playground.js
+++ src/js/playground.js
@@ -1,17 +1,84 @@
 require([
     '$',
     'pinny',
-    'sheet-right'
+    'sheet-right',
+    'fixed-sticky',
+    'jquery-sticky'
 ],
 function(
     $,
     Pinny,
-    sheetRight
+    sheetRight,
+    FixedSticky,
+    jQuerySticky
 ) {
-    var _playgroundUI = function() {
-        console.log('Playground UI');
+    var $header = $('.playground-header');
+    var $footer = $('.playground-footer');
+    var $clearStickyHeader = $('.js-clear-sticky-header');
+    var $clearStickyFooter = $('.js-clear-sticky-footer');
+
+    var $fixedStickyHeaderTrigger = $('.js-header-sticky__fixed-sticky');
+    var $fixedStickyFooterTrigger = $('.js-footer-sticky__fixed-sticky');
+    var $jqueryStickyHeaderTrigger = $('.js-header-sticky__stickyjs');
+    var $jqueryStickyFooterTrigger = $('.js-footer-sticky__stickyjs');
+
+    var _bindFixedSticky = function() {
+        $fixedStickyHeaderTrigger.on('click', function(e) {
+            e.preventDefault();
+            $header.addClass('fixedsticky fixedsticky__header');
+            $header.fixedsticky();
+            $clearStickyHeader.removeAttr('hidden');
+
+            $jqueryStickyHeaderTrigger.attr('hidden', true);
+            $jqueryStickyFooterTrigger.attr('hidden', true);
+        });
+
+        $fixedStickyFooterTrigger.on('click', function(e) {
+            e.preventDefault();
+            $footer.addClass('fixedsticky fixedsticky__footer');
+            $footer.fixedsticky();
+            $clearStickyFooter.removeAttr('hidden');
+
+            $jqueryStickyHeaderTrigger.attr('hidden', true);
+            $jqueryStickyFooterTrigger.attr('hidden', true);
+        });
+
+
+        // Clearing bindings
+        $clearStickyHeader.on('click', function(e) {
+            e.preventDefault();
+            $header.removeClass();
+            $header.fixedsticky('destroy');
+            $clearStickyHeader.attr('hidden', true);
+
+            $jqueryStickyHeaderTrigger.removeAttr('hidden');
+            $jqueryStickyFooterTrigger.removeAttr('hidden');
+        });
+
+        $clearStickyFooter.on('click', function(e) {
+            e.preventDefault();
+            $footer.removeClass();
+            $footer.fixedsticky('destroy');
+            $clearStickyFooter.attr('hidden', true);
+
+            $jqueryStickyHeaderTrigger.removeAttr('hidden');
+            $jqueryStickyFooterTrigger.removeAttr('hidden');
+        });
     };
 
-    _playgroundUI();
+    var _bindJquerySticky = function() {
+
+    };
+
+    var _playgroundUI = function() {
+        console.log('Playground UI');
+
+        _bindFixedSticky();
+        _bindJquerySticky();
+    };
+
+    $(function() { // doc ready
+        _playgroundUI();
+    });
 }, undefined, true);
 // relPath, forceSync
